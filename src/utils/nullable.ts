950d94ae58a097458329b0f3560e96c5461bbc27
Alessandro Vitali <alessandro.vitali@coachhub.com>
2024-05-17 17:44:23 +0200

    setup the project

---
+++ src/utils/nullable.ts
@@ -0,0 +1,14 @@
+/**
+ * Utility class that wraps a nulluble value.
+ * It's required to work together the Result class.
+ */
+export class Nullable<T> {
+  #value: T | null;
+  constructor(value: T | null) {
+    this.#value = value;
+  }
+
+  get value(): T | null {
+    return this.#value;
+  }
+}
